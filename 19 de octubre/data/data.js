35b6f2e9b37a1a09f67153d004e07e2603c600e5
Nieto$ <127440135+JulietaNieto@users.noreply.github.com>
2023-10-19 16:20:40 -0500

    Add files via upload

---
+++ 19 de octubre/data/data.js
@@ -0,0 +1,33 @@
+export  const data =
+[
+    {
+        "id":1,
+        "name": "esmeraldas",
+        "image": "/img/1.jpg"
+    },     {
+        "id":2,
+        "name":"The good kid a mad city",
+        "image": "/img/2.jpg"
+    }, 
+    {
+        "id":3,
+        "name": "Arketipo temporada 1",
+        "image": "/img/3.jpg"
+    }, 
+    {
+        "id":4,
+        "name": "Arketipo temporada 2",
+        "image": "/img/4.jpg"
+    }, 
+    {
+        "id":5,
+        "name": "Juegos de azar",
+        "image": "/img/5.jpg"
+    }, 
+    {
+        "id":6,
+        "name": "Servicios AmbulatorioZ",
+        "image": "/img/6.jpg"
+    }, 
+
+]
